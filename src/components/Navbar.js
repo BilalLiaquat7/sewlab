9aa3834ce050445b1a761376624b1c5f4887a394
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-21 13:00:21 +0500

    Navbar component added with basic menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="bg-white">
+        <ul className="px-2 py-6 flex space-x-16 justify-end mr-96">
+          <li>HOME</li>
+          <li>ABOUT US</li>
+          <li>CLIENTS</li>
+          <li>SERVICES</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}

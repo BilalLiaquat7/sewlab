bf0da6fde2f047bed3cf3caeeccaa484815e6ca6
sheezu900 <shahzamanlive2@gmail.com>
2023-08-23 19:16:16 +0500

    add home js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+// import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
+      <Home />
     {/* <Navbar /> */}
-    <Hero />
+    {/* <Hero /> */}
     </div>
   );
 }

892092ca7f67be5bc847d76019f9f1ed199fe90c
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-09-03 18:42:15 +0500

    accordian design fix

--- src/components/headwear/productdetail.js
+++ src/components/headwear/productdetail.js
@@ -1,30 +1,38 @@
+import { useState } from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Styles from  './styles';
+import Styles from "./styles";
 
 const ProductDetail = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   return (
     <>
-      <div className="mt-16 w-11/12 md:hidden">
+      <div className="mt-16 pb-1 w-11/12 md:hidden">
         {/* Fundamentals */}
         <Accordion
+          expanded={expanded === "panel1"}
+          onChange={handleChange("panel1")}
+          elevation={0}
           sx={{
             backgroundColor: "transparent",
             color: "white",
-            padding: "0"
+            boxShadow: '0 1px 0 0 rgba(255, 255, 255, 0.15)'
           }}
         >
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon
-              sx={{ color: "white" }}
-            />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
           >
-            <Typography>Fundamentals</Typography>
+            <Typography variant="h6">Fundamentals</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
@@ -56,25 +64,28 @@ const ProductDetail = () => {
         </Accordion>
         {/* How to Order */}
         <Accordion
+          expanded={expanded === "panel2"}
+          onChange={handleChange("panel2")}
           sx={{
             backgroundColor: "transparent",
             color: "white",
+            boxShadow: '0 1px 0 0 rgba(255, 255, 255, 0.15)'
           }}
         >
           <AccordionSummary
             expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
-            aria-controls="panel2a-content"
-            id="panel2a-header"
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
           >
-            <Typography>How to order ?</Typography>
+            <Typography variant="h6">How to order ?</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
               <div>
                 <span>01. Submit Design</span>
                 <br />
-                Our client offers design templates and the option to work with our
-                department to create customized ar work.
+                Our client offers design templates and the option to work with
+                our department to create customized ar work.
               </div>
               <div>
                 <br />
@@ -93,9 +104,9 @@ const ProductDetail = () => {
           </AccordionDetails>
         </Accordion>
       </div>
-      <div className='md:mt-24 md:w-[95%] w-11/12  md:flex '>
+      <div className="md:mt-24 md:w-[95%] w-11/12  md:flex ">
         <Styles />
-        <div className='md:w-[90%] hidden'>
+        <div className="md:w-[90%] hidden">
           <h1>product detail</h1>
         </div>
       </div>

6551ccfc15d7d348a6972f2872580d85b57dc7ab
sheezu900 <shahzamanlive2@gmail.com>
2023-09-03 19:36:03 +0500

    complete desktop page

--- src/components/headwear/productdetail.js
+++ src/components/headwear/productdetail.js
@@ -8,7 +8,7 @@ import Styles from  './styles';
 const ProductDetail = () => {
   return (
     <>
-      <div className="mt-16 w-11/12 md:hidden">
+      <div className="mt-16 mb-4 w-11/12 md:hidden">
         {/* Fundamentals */}
         <Accordion
           sx={{
@@ -93,10 +93,53 @@ const ProductDetail = () => {
           </AccordionDetails>
         </Accordion>
       </div>
-      <div className='md:mt-24 md:w-[95%] w-11/12  md:flex '>
+      <div className='md:mt-16 md:w-[95%] w-11/12 md:gap-4  md:flex '>
         <Styles />
-        <div className='md:w-[90%] hidden'>
-          <h1>product detail</h1>
+        <div className='md:w-[90%] hidden md:flex md:flex-col md:pt-16 md:gap-4'>
+          <div className='flex w-full gap-4'>
+            <div className='flex flex-1 p-4 flex-col rounded-lg bg-black/20 bg-blend-soft-light gap-2 text-white'>
+              <h1 className='text-base font-bold'>Fundamentals</h1>
+              <div className='gap-3'>
+                <div className='p-6 flex flex-col gap-2'>
+                  <h2 className='text-base font-bold'>Minimum Order</h2>
+                  <p>72 pc total order (24 pc per colorway)</p>
+                </div>
+                <div className='p-6 flex flex-col gap-2'>
+                  <h2 className='text-base font-bold'>Setup Fees</h2>
+                  <p>$38.00 (up to 20K stitches)</p>
+                </div>
+                <div className='p-6 flex flex-col gap-2'>
+                  <h2 className='text-base font-bold'>Sample Fees</h2>
+                  <p>72 pc total order (24 pc per colorway)</p>
+                </div>
+                <div className='p-6 flex flex-col gap-2'>
+                    <h2>Sample Leadtime</h2>
+                    <p>10 days*</p>
+                </div>
+              </div>
+            </div>
+            <div className='flex flex-1 p-4 flex-col rounded-lg bg-black/20 bg-blend-soft-light gap-2 text-white'>
+              <h1 className='text-base font-bold'>How to order ?</h1>
+              <div className='gap-3'>
+                <div className='p-6 flex flex-col gap-2'>
+                  <h2 className='text-base font-bold'>01. Submit Design</h2>
+                  <p>Our Company offers design templates and the option to work with our art department to create customized artwork.</p>
+                </div>
+                <div className='p-6 flex flex-col gap-2'>
+                  <h2 className='text-base font-bold'>02. Price Quote</h2>
+                  <p>Once your artwork is finalized you’ll a receive price quote.</p>
+                </div>
+                <div className='p-6 flex flex-col gap-2'>
+                  <h2 className='text-base font-bold'>03. Production</h2>
+                  <p>After finalizing payment for your order, we will ensure proper preparation for production with a physical sample for approval</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col p-3 rounded-lg self-stretch bg-black/20'>
+            <p className='text-white'><span>*</span> Lead times are approximate and vary throughout the year if you have a deadline, please consult with your account manager in   advance</p>
+            <p className='text-white'><span>*</span> Production lead time is after sample approval</p>
+          </div>
         </div>
       </div>
     </>

0d1fa9873541315f27a158013634b03f93c6ceb2
sheezu900 <shahzamanlive2@gmail.com>
2023-09-03 16:52:47 +0500

    styles desktop view ma bana li hia

--- src/components/headwear/Navbar.js
+++ src/components/headwear/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
   return (
     <div className="text-black md:justify-between md:items-center top-4 md:top-0 absolute flex mx-auto w-[95%] z-10 md:z-0 ">
 
-      <button className="hidden bg-black text-white text-xl md:flex px-8 py-1">Log in</button>
+      <button className="hidden md:bg-black md:text-white md:text-xl md:flex px-8 py-2 rounded-md">Log in</button>
         <img
         className="left-4 top-2 h-10"
         src={logoNav}
--- src/components/headwear/product.js
+++ src/components/headwear/product.js
@@ -2,19 +2,19 @@ import cap from "../../assets/cap.png";
 
 const Product = () => {
   return (
-    <div>
+    <div className='md:w-[10%]'>
     <h1 className="text-white text-2xl mt-4">Styles</h1>
       <div className="flex pt-4 gap-[4%] flex-wrap items-center">
-        <div className="flex w-[48%] py-2 my-2 px-4 justify-center items-center rounded-lg bg-[#00000042] bg-blend-soft-light backdrop-blur-sm">
+        <div className="flex w-[48%] md:w-[100%] py-2 my-2 px-4 justify-center items-center rounded-lg bg-[#00000042] bg-blend-soft-light backdrop-blur-sm">
           <img src={cap} alt="" className="w-auto h-full" />
         </div>
-        <div className="flex w-[48%] py-2 my-2 px-4 justify-center items-center rounded-lg bg-[#00000042] bg-blend-soft-light backdrop-blur-sm">
+        <div className="flex w-[48%] md:w-[100%] py-2 my-2 px-4 justify-center items-center rounded-lg bg-[#00000042] bg-blend-soft-light backdrop-blur-sm">
           <img src={cap} alt="" className="w-auto h-full" />
         </div>
-        <div className="flex w-[48%] py-2 my-2 px-4 justify-center items-center rounded-lg bg-[#00000042] bg-blend-soft-light backdrop-blur-sm">
+        <div className="flex w-[48%] md:w-[100%]  py-2 my-2 px-4 justify-center items-center rounded-lg bg-[#00000042] bg-blend-soft-light backdrop-blur-sm">
           <img src={cap} alt="" className="w-auto h-full" />
         </div>
-        <div className="flex w-[48%] py-2 my-2 px-4 justify-center items-center rounded-lg bg-[#00000042] bg-blend-soft-light backdrop-blur-sm">
+        <div className="flex w-[48%] md:w-[100%]  py-2 my-2 px-4 justify-center items-center rounded-lg bg-[#00000042] bg-blend-soft-light backdrop-blur-sm">
           <img src={cap} alt="" className="w-auto h-full" />
         </div>
       </div>

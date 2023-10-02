7915977cc8664ed632c60beffd1235cbbf0af4b0
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-10-02 11:50:42 +0500

    colors added with other view

---
+++ src/components/apparel/data.js
@@ -0,0 +1,78 @@
+const colorImages = [
+  {
+    number: 1,
+    image: "color1",
+  },
+  {
+    number: 2,
+    image: "color2",
+  },
+  {
+    number: 3,
+    image: "color3",
+  },
+  {
+    number: 4,
+    image: "color4",
+  },
+  {
+    number: 5,
+    image: "color5",
+  },
+  {
+    number: 6,
+    image: "color6",
+  },
+  {
+    number: 7,
+    image: "color7",
+  },
+  {
+    number: 8,
+    image: "color8",
+  },
+  {
+    number: 9,
+    image: "color9",
+  },
+  {
+    number: 10,
+    image: "color10",
+  },
+  {
+    number: 11,
+    image: "color11",
+  },
+  {
+    number: 12,
+    image: "color12",
+  },
+  {
+    number: 13,
+    image: "color13",
+  },
+  {
+    number: 14,
+    image: "color14",
+  },
+  {
+    number: 15,
+    image: "color15",
+  },
+  {
+    number: 16,
+    image: "color16",
+  },
+  {
+    number: 17,
+    image: "color17",
+  },
+  {
+    number: 18,
+    image: "color18",
+  },
+  {
+    number: 19,
+    image: "color19",
+  },
+];
---
+++ src/components/apparel/styles.js
@@ -0,0 +1,7 @@
+import Product from "./product";
+
+const Styles = (props) => {
+  return <Product handleSelection={props.handleSelection} />;
+};
+
+export default Styles;
